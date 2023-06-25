08d24c5374b083196e66117914dd7337a0ec8834
Betsy-Nazareno <belinaza@espol.edu.ec>
2023-06-25 11:30:11 -0500

    Changes

--- src/modules/Spreadsheets/datasets/example.ts
+++ src/modules/Spreadsheets/datasets/example.ts
@@ -7,18 +7,11 @@ export const getExampleData = () => {
         m: "nombre",
         v: "Nombre",
         bl: 1,
-        bg: "#fff000",
       },
       {
         ct: { fa: "General", t: "g" },
         m: "apellido",
         v: "Apellido",
-        bl: 1,
-      },
-      {
-        ct: { fa: "General", t: "g" },
-        m: "example",
-        v: "Example",
         bl: 1,
       },
       ...cells,
@@ -46,19 +39,6 @@ export const getExampleData = () => {
         ct: { fa: "General", t: "g" },
         m: "Carreño",
         v: "Carreño",
-      },
-      ...cells,
-    ],
-    [
-      {
-        ct: { fa: "General", t: "g" },
-        m: "nuevo",
-        v: "Nuevo",
-      },
-      {
-        ct: { fa: "General", t: "g" },
-        m: "dos",
-        v: "Dos",
       },
       ...cells,
     ],
--- src/modules/Spreadsheets/datasets/index.ts
+++ src/modules/Spreadsheets/datasets/index.ts
@@ -4,7 +4,6 @@ import { getExampleData } from "./example";
 const dataSets = {
   example: getExampleData(),
   customers: generateCustomers(),
-  tiendas: generateCustomers(),
 };
 
 export default dataSets as any;
